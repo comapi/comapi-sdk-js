3f60622ed7003a3cea9b192689f01ac5da21b93e
Univibe69 <stevan@dyn-l-mac-sl1.local>
2017-06-20 16:15:12 +0100

    added a dynamic test

--- specs/container.spec.ts
+++ specs/container.spec.ts
@@ -125,4 +125,51 @@ describe("IOC tests", () => {
 
   });
 
+
+
+  it("Should fill in dynamic dependencies", () => {
+
+    interface ILogger {
+      log(string): void;
+    }
+
+    class Logger implements ILogger {
+      public log(message) {
+        console.log(message);
+      }
+    }
+    let logId = "Logger";
+
+    interface IClown {
+      juggle(): void;
+    }
+
+    @injectable()
+    class Clown implements IClown {
+
+      @inject(logId) private _logger: Logger;
+
+      public juggle() {
+        this._logger.log("Im juggling");
+      }
+
+    }
+
+    let clownId = "Clown";
+
+    let container = new Container();
+    container.bind<ILogger>(logId).toDynamicValue((context) => {
+      console.log("context", context);
+      return new Logger();
+    });
+    container.bind<IClown>(clownId).to(Clown);
+
+    let clown = container.get<IClown>(clownId);
+
+    expect(clown).toBeDefined();
+
+    clown.juggle();
+
+  });
+
 });
